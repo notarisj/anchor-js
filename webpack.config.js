d2f765fc15c0f9b4e2afe60f5bee7abaacaded80
notaris <80954329+notarisj@users.noreply.github.com>
2024-07-09 01:59:11 +0300

    minor configs for github page

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // You can switch to 'production' later
-    entry: './src/anchor.js',  // Entry point of your application
+    mode: 'development', // or 'production'
+    entry: './src/anchor.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true, // This option will clean the output directory before emit
     },
     module: {
         rules: [
             {
-                test: /\.js$/, // Use Babel for transpiling modern JS
+                test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                use: 'babel-loader',
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body', // Ensure scripts are injected in the body
+        })
+    ],
 };
